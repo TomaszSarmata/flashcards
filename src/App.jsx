2c62d5c6921a5f29b032d40220f0e47e447a2219
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-08-21 14:45:34 +0100

    create FlashCard component to render in the main App

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [reversed, setReversed] = useState(false);
+
+  const handleClick = () => {
+    setReversed(!reversed);
+  };
   return (
     <div className="main-container">
-      <div className="card card-1">
-        <p className="card-paragraph">What language is React based on?</p>
-      </div>
-      <div className="card card-2">
-        <p className="card-paragraph">
-          What are the building blocks of React apps?
-        </p>
-      </div>
-      <div className="card card-3 backside">
-        <p className="card-paragraph">
-          What's the name of the syntax we use to describe an UI in React?
-        </p>
-      </div>
-      <div className="card card-4">
-        <p className="card-paragraph">
-          How to pass data from parent to child component?
-        </p>
-      </div>
-      <div className="card card-5">
-        <p className="card-paragraph">How to give components memeory?</p>
-      </div>
-      <div className="card card-6">
-        <p className="card-paragraph">
-          What do we call an input element taht is completely synchronised with
-          state?
-        </p>
-      </div>
+      <FlashCard text="What language is React based on?" />
+      <FlashCard text="What are the building blocks of React apps?" />
+      <FlashCard text="What's the name of the syntax we use to describe an UI in React?" />
+      <FlashCard text="How to pass data from parent to child component?" />
+      <FlashCard text="How to give components memory?" />
+      <FlashCard
+        text="What do we call an input element that is completely synchronised with
+          state?"
+      />
+    </div>
+  );
+}
+
+function FlashCard({ text }) {
+  const [reversed, setReversed] = useState(false);
+
+  const handleClick = () => {
+    setReversed(!reversed);
+  };
+
+  return (
+    <div
+      className={`card ${reversed ? 'backside' : ''}`}
+      onClick={handleClick}>
+      <p className="card-paragraph">{text}</p>
     </div>
   );
 }
