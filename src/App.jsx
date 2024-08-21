226e4a6c754b86683eee0d7fec9c8273a0e80625
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-08-21 13:46:10 +0100

    implemented simple UI

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,32 @@ import './App.css';
 function App() {
   return (
     <div className="main-container">
-      <div>
-        <p>What language is React based on?</p>
+      <div className="card card-1">
+        <p className="card-paragraph">What language is React based on?</p>
+      </div>
+      <div className="card card-2">
+        <p className="card-paragraph">
+          What are the building blocks of React apps?
+        </p>
+      </div>
+      <div className="card card-3 backside">
+        <p className="card-paragraph">
+          What's the name of the syntax we use to describe an UI in React?
+        </p>
+      </div>
+      <div className="card card-4">
+        <p className="card-paragraph">
+          How to pass data from parent to child component?
+        </p>
+      </div>
+      <div className="card card-5">
+        <p className="card-paragraph">How to give components memeory?</p>
+      </div>
+      <div className="card card-6">
+        <p className="card-paragraph">
+          What do we call an input element taht is completely synchronised with
+          state?
+        </p>
       </div>
     </div>
   );
