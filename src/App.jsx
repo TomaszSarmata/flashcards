32218759ef3319bfd521a6d681a8f9f4323109d6
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-08-21 13:28:13 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="main-container">
+      <div>
+        <p>What language is React based on?</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
